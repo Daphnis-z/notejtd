f5aa90d658a112bd50bbcd7403f54a164f31de8b
Daphnis-work <daphnisz@163.com>
2018-11-29 15:43:16 +0800

    add google guava

---
+++ google-guava/guava-demo/guavaDemo/src/main/java/com/daphnis/guava/GuavaDemo.java
@@ -0,0 +1,46 @@
+package com.daphnis.guava;
+
+import java.util.List;
+
+import com.google.common.base.Strings;
+import com.google.common.collect.Lists;
+
+/**
+ * Hello world!
+ *
+ */
+public class GuavaDemo 
+{
+	
+	public static void listDemo() {
+		List<String> list=Lists.newArrayList("hello","guava");
+        System.out.println( "create a list by guava: "+String.join(" ", list) );
+	}
+	
+	public static void stringDemo() {
+		String str1="Daphnis";
+		if (Strings.isNullOrEmpty(str1)) {
+			System.out.println("str1 is null or empty..");
+		}else{
+			System.out.println("str1 is "+str1);
+		}
+		
+		String str2=null;
+		if (Strings.isNullOrEmpty(str2)) {
+			System.out.println("str2 is null or empty..");
+		}else{
+			System.out.println("str2 is "+str2);
+		}
+	}
+	
+    public static void main( String[] args )
+    {
+    	listDemo();
+    	stringDemo();
+    }
+    
+}
+
+
+
+
